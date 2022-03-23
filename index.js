0c0b170077e2fd00dad906d78f9a24364b483d61
zeeshan ahamd <zeeshan@craxxino.com>
2022-03-23 12:07:21 +0530

    backend add

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
-const express = require('express');
-const app = express();
-const cors=require('cors');
-const db=require('./config/db');
-const { getAllPizzas } = require('./controllers/pizza');
-const pizzasRouter = require("./routes/pizzas");
-const usersRouter = require("./routes/users");
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const db = require('./config/db')
+const { getAllPizzas } = require('./controllers/pizza')
+const pizzasRouter = require('./routes/pizzas')
+const usersRouter = require('./routes/users')
 
-db();
+db()
 // Middlewares
-app.use(express.json());
-app.use(cors());
-app.use("/api/v1", pizzasRouter);
-app.use("/api/v1/auth", usersRouter);
+app.use(express.json())
+app.use(cors())
+app.get('/', (req, res) => {
+  res.send('Hello World')
+})
+app.use('/api/v1', pizzasRouter)
+app.use('/api/v1/auth', usersRouter)
 app.listen(9000, () => {
-    console.log("Server started on port 9000");
-});
+  console.log('Server started on port 9000')
+})
